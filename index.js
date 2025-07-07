e6856812d5120285ffb7e4e9012808b9c1d7d9cd
Neko <90152301+neko3703@users.noreply.github.com>
2025-07-07 20:45:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ const app = express();
 app.use('',express.static(path.join(__dirname, 'public')));
 
 app.get('/', (request, response) => {
-	return response.sendFile('index.html', { root: '.' });
+  return response.sendFile('index.html', { root: '.' });
 });
 
 app.get('/auth/discord', (request, response) => {
-	return response.sendFile('index.html', { root: 'https://nekocode.in/' });
+  return response.sendFile('index.html', { root: 'https://nekocode.in/' });
+});
+
+app.get("/discord", (req, res) => {
+  res.redirect("https://discord.gg/Xyk2TjeAMJ");
 });
 
 const port = '53134';
